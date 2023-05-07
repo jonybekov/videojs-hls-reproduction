81bc7f489c529d950be99d6b89f5518ab8e81e2d
jonybekov <39439363+jonybekov@users.noreply.github.com>
2023-05-08 00:12:16 +0300

    remove redundant targets from browserlist

--- vite.config.js
+++ vite.config.js
@@ -44,13 +44,6 @@ export default defineConfig({
         'Chrome >= 53',
         'not IE 11',
         'not dead',
-        'not ie 11',
-        'not baidu 7',
-        'not and_qq 11',
-        'not and_uc 12',
-        'not kaios 2',
-        'not op_mini all',
-        'not op_mob 64',
       ], // Apply polyfills to support older browsers. See https://webostv.developer.lge.com/develop/specifications/web-api-and-web-engine#web-engine
     }),
     legacyScriptsPlugin(),
