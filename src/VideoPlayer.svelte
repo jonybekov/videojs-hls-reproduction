830a00ecdcb1fccf4c071a0f664535e01cbdcdc1
jonybekov <39439363+jonybekov@users.noreply.github.com>
2023-05-08 00:23:12 +0300

    cleanup

--- src/VideoPlayer.svelte
+++ src/VideoPlayer.svelte
@@ -1,33 +1,43 @@
 <script>
 import {onMount} from "svelte"
 import videojs from "video.js"
+
+let src = "https://cdn.voxe.tv/s3/movies/shazam-fury-of-the-gods-903115/master.m3u8" 
+let player;
+
 const options = {
   autoplay: "any",
   aspectRatio: "16:9",
-  fluid: true,
-      textTrackSettings: true,
-
-      vhs: {
-        overrideNative: true,
-      },
-      html5: {
-        hlsjsConfig: {
-          debug: true,
-        },
-      },
-
+  fluid: false,
+  textTrackSettings: true,
+  vhs: {
+    overrideNative: true,
+  },
+  html5: {
+    hlsjsConfig: {
+      debug: true,
+    },
+  },
 }
+
 onMount(() => {
-const player = videojs('#video', options);
+  player = videojs('#video', options);
 
-    player.src({
-      src: "https://cdn.voxe.tv/s3/movies/shazam-fury-of-the-gods-903115/master.m3u8",
-      type: "application/x-mpegURL",
-    })
-
-
+  player.src({
+    src: "https://cdn.voxe.tv/s3/movies/shazam-fury-of-the-gods-903115/master.m3u8",
+    type: "application/x-mpegURL",
+  })
 })
+
+$: {
+  if(player) player.src({
+    src: src,
+    type: "application/x-mpegURL"
+  })
+}
+
 </script>
 
-<video id="video" class="video-js vjs-theme-city vjs-big-play-centered" controls />
+<input bind:value={src} />
+<video id="video" class="video-js vjs-theme-city vjs-big-play-centered" width="800" height="800" controls />
 
